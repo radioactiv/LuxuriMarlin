7ac2ef73499803bc652cf057afdfc36b3e51a06b
Richard Layton <rlayton@gmail.com>
2023-01-18 21:35:03 -0700

    Adjust Z feedrate

--- Marlin/Configuration_Tenlog.h
+++ Marlin/Configuration_Tenlog.h
@@ -4,15 +4,13 @@
 //Bed
 #if ENABLED(ACBed)
     #define LUX_TEMP_SENSOR_BED 11 //AC Bed
-#else
-    //Stock
+#else //Stock
     #define LUX_TEMP_SENSOR_BED 1
 #endif
 
 //Extruder
 #if ENABLED(TitanExtruder)
     #define LUX_DEFAULT_AXIS_STEPS_PER_UNIT { 80, 80, 800, 382.17, 382.17 }
-#else 
-    //Stock
+#else //Stock
     #define LUX_DEFAULT_AXIS_STEPS_PER_UNIT { 80, 80, 800, 92.6, 92.6 }
 #endif
