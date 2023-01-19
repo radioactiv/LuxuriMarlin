386c93c5e430ee26925b7207c358c5ea9cbef56b
Richard Layton <rlayton@gmail.com>
2023-01-18 20:27:21 -0700

    Configure esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   LUX_DEFAULT_AXIS_STEPS_PER_UNIT
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
--- Marlin/Configuration_Tenlog.h
+++ Marlin/Configuration_Tenlog.h
@@ -1,2 +1,18 @@
+#define ACBed
 #define TitanExtruder
-#define LUX_TEMP_SENSOR_BED 11 //AC Bed
+
+//Bed
+#if ENABLED(ACBed)
+    #define LUX_TEMP_SENSOR_BED 11 //AC Bed
+#else
+    //Stock
+    #define LUX_TEMP_SENSOR_BED 1
+#endif
+
+//Extruder
+#if ENABLED(TitanExtruder)
+    #define LUX_DEFAULT_AXIS_STEPS_PER_UNIT { 80, 80, 800, 382.17, 382.17 }
+#else 
+    //Stock
+    #define LUX_DEFAULT_AXIS_STEPS_PER_UNIT { 80, 80, 800, 92.6, 92.6 }
+#endif
