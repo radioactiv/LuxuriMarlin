dc86ca59a6c187355ecafa0d3863092ae5a0c9cb
Richard Layton <rlayton@gmail.com>
2023-01-21 16:17:23 -0700

    Implement safety unpark by Murdock

--- Marlin/Configuration_Tenlog.h
+++ Marlin/Configuration_Tenlog.h
@@ -1,14 +1,17 @@
 #define ACBed
 #define TitanExtruder
+#define SAFETY_Z_UNPARK 4.00 // 26/04/2021 Murdock avoid bed clips (Height to raise. Set to 0 for disable).
+#define SAFETY_Y_UNPARK 15.00 // 26/04/2021 Murdock avoid bed clips (Distance to move on Y axis for avoid bed clips).
+
 #define HAS_STATUS_MESSAGE 1
 #define REPORT_FAN_CHANGE
 #define BABYSTEP_HOME_Z_OFFSET
+
 #define LUX_X_MIN_POS -47
 #define LUX_X2_MIN_POS 15
 #define LUX_X2_MAX_POS 279
 #define LUX_DEFAULT_DUPLICATION_X_OFFSET 117
 #define LUX_NUM_RUNOUT_SENSORS 1
-
 
 //Bed
 #if ENABLED(ACBed)
